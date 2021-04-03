d94454ee1aee37d889355085890f394d6d4bd92f
Jonathon Carstens <jon.carstens@gmail.com>
2021-04-03 10:54:47 -0700

    Finished checking distance from point to Polygon and printing random sample of results.

--- E1-7.py
+++ E1-7.py
@@ -3,6 +3,8 @@
 import MME565
 import numpy as np
 
+random_samples = 5
+
 # computeDistanceToPolygon
 
 polygon_vertices = [
@@ -15,31 +17,32 @@ polygon_vertices = [
         [1, 5],
     ]
 
-star_vertices = [
-    [2, 2],
-    [4, 7],
-    [6, 2],
-    [1, 5],
-    [7, 5],
-]
+polygon = MME565.Polygon(polygon_vertices)
 
-point_test = []
+p_q = []
 
-for point in star_vertices:
-    point_test.append(MME565.Point(point))
+for x in np.linspace(0, 9, 25):
+    for y in np.linspace(0, 9, 25):
+        p_q.append([x, y])
 
-polygon = MME565.Polygon(point_test)
-
-points = []
-for _ in range(1000):
-    points.append(MME565.Point([np.random.random() * 10, np.random.random() * 10]))
+# computeDistancePointToPolygon
 
 distances = []
 
-for point in points:
-    if polygon.check_point_inside_polygon(point):
-        distances.append([0, "point inside polygon"])
-    else:
-        distances.append([polygon.distance_point_to_polygon(point), None])
+for point in p_q:
+    distances.append([polygon.distance_point_to_polygon(point), point])
 
-MME565.show_polygon(polygon, points)
+print(distances[-1])
+
+print(f"{len(p_q)} points checked against {polygon.num_sides} polygon segments.")
+
+for _ in range(random_samples):
+    rand_dist = distances[np.random.randint(0, len(distances))]
+    rand_dist_dist = f"Distance: {rand_dist[0][0][0]} \n"
+    rand_dist_q = f"q: {rand_dist[1]}. \n"
+    rand_dist_seg = f"{rand_dist[0][1]}"
+    rand_dist_str = rand_dist_dist + rand_dist_q + rand_dist_seg
+    print(f"Random sample point to polygon: \n{rand_dist_str} \n")
+
+
+MME565.show_polygon(polygon, p_q)

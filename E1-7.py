7ac99654066ed26fbbc39ab485b6c7e72a6e7536
Jonathon Carstens <jon.carstens@gmail.com>
2021-04-03 17:20:23 -0700

    Finished file

--- E1-7.py
+++ E1-7.py
@@ -2,6 +2,9 @@
 
 import MME565
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
 
 random_samples = 5
 
@@ -21,8 +24,8 @@ polygon = MME565.Polygon(polygon_vertices)
 
 p_q = []
 
-for x in np.linspace(0, 9, 25):
-    for y in np.linspace(0, 9, 25):
+for x in np.linspace(0, 9, 12):
+    for y in np.linspace(0, 9, 12):
         p_q.append([x, y])
 
 # computeDistancePointToPolygon
@@ -31,8 +34,6 @@ distances = []
 
 for point in p_q:
     distances.append([polygon.distance_point_to_polygon(point), point])
-
-print(distances[-1])
 
 print(f"{len(p_q)} points checked against {polygon.num_sides} polygon segments.")
 
@@ -44,5 +45,34 @@ for _ in range(random_samples):
     rand_dist_str = rand_dist_dist + rand_dist_q + rand_dist_seg
     print(f"Random sample point to polygon: \n{rand_dist_str} \n")
 
+tangent_vectors = []
+vectors = []
 
-MME565.show_polygon(polygon, p_q)
+for point in range(len(distances)):
+    vectors.append([distances[point][0][1].vector_point_to_segment(distances[point][1]), distances[point][1]])
+    tangent_vectors.append([distances[point][0][1].tangent_vector_point_to_segment(distances[point][1]), distances[point][1]])
+
+
+# MME565.show_polygon(polygon, p_q)
+
+fig, ax = plt.subplots()
+patches = [mpatches.Polygon(polygon.vertices_list)]
+colors = np.linspace(0, 1, 1)
+collection = PatchCollection(patches, cmap=plt.cm.hsv, alpha=0.3)
+collection.set_array(colors)
+ax.add_collection(collection)
+i = 0
+if type(p_q[0]) != list:
+    q = [p_q]
+for point in p_q:
+    if type(point) != MME565.Point:
+        point = MME565.Point(point)
+    if polygon.check_point_inside_polygon(point):
+        plt.plot(point.x, point.y, "bo")
+    else:
+        plt.plot(point.x, point.y, "rx")
+        q = ax.quiver(vectors[i][1][0], vectors[i][1][1], vectors[i][0].x, vectors[i][0].y)
+    i += 1
+plt.axis('equal')
+plt.tight_layout()
+plt.show()
